09f798ed7ab00e0908e774b794d398cea32957eb
andrepsavoie-rgb <andrep.savoie@gmail.com>
2026-01-29 21:51:41 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,29 @@
-const CACHE_NAME = 'decompte-pwa-v1';
-const FILES_TO_CACHE = [
+const CACHE_NAME = 'decompte-cache-v1';
+const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/manifest.json',
+  '/countdown.js',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
 ];
 
-// Installation du service worker
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
 
-// Interception des requêtes
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+// Exemple simple pour mise à jour toutes les minutes
+self.addEventListener('periodicsync', event => {
+  if (event.tag === 'update-countdown') {
+    event.waitUntil(
+      fetch('/index.html') // Recharge les données pour le widget
+    );
+  }
 });
