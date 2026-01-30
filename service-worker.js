d04fc05e0c433db5036650efd76b2d24843eef1b
andrepsavoie-rgb <andrep.savoie@gmail.com>
2026-01-29 22:09:45 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'decompte-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/widget.html',
-  '/manifest.json',
-  '/countdown.js',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/decompte/',
+  '/decompte/index.html',
+  '/decompte/widget.html',
+  '/decompte/manifest.json',
+  '/decompte/countdown.js',
+  '/decompte/icons/icon-192.png',
+  '/decompte/icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -15,7 +15,6 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
